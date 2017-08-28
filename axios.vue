c71ef72eb64bf152bfeb81c9d7570369811513d6
HuangChongqing <1756260160@qq.com>
2017-08-28 20:49:30 +0800

    text-overflow:ellipsis;多行文本省略

---
+++ axios.vue
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+	created () {
+		axios.post('/user',{
+			first: "Huang",
+			last: "Chongqing"
+		})
+		.then(function (res) {
+			res.data
+		})
+		.catch(function (error) {
+			console.log(error)
+		})
+	}
+</script>
